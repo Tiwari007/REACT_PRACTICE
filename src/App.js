6f568db4459f096a68318a03ae9174e9a1cd49b7
Tiwari007 <vt2174781@gmail.com>
2022-02-02 17:54:01 +0530

    Remove Extra Space

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link
-} from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoute from './AppRoute'
 function App() {
   return (
     <Router>
     <div className="App">
-      
         <AppRoute />
-      
-      
-      
-      
     </div>
     </Router>
   );
